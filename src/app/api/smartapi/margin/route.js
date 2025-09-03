de069273f1f14e527b5c2b8c399867d39dda8b08
shreyas-sketch <shreyas@allianceoftitans.com>
2025-09-03 14:17:23 +0530

    Update route.js

--- src/app/api/smartapi/margin/route.js
+++ src/app/api/smartapi/margin/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
     const baseUrl =
       process.env.NODE_ENV === "development"
         ? "http://localhost:8888"
-        : process.env.SITE_URL; // set this in Netlify settings
+        : "https://margin.bullforce.ai/"; // set this in Netlify settings
 
     const sessionRes = await fetch(`${baseUrl}/.netlify/functions/ensureSession`, {
       method: "GET",
@@ -53,6 +53,7 @@ export async function POST(req) {
     );
   }
 }
+
 
 
 
